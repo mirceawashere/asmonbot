1534dffdcd0609614083fea6c9f2459fb5ed354c
mirceawashere <96563222+mirceawashere@users.noreply.github.com>
2025-02-02 20:04:00 +0200

    golive

--- asmonbot.py
+++ asmonbot.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import os
 import google.generativeai as genai
 import requests
+import random
 from requests_oauthlib import OAuth1
 
 app = FastAPI()
@@ -17,7 +18,13 @@ google_key = os.getenv("GOOGLE_API")
 def post_to_X():
     genai.configure(api_key=google_key)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    answer = model.generate_content("You're Asmongold, famous Twitch streamer. Be full of yourself, funny, ironic, snarky, be merciless.  Use his lingo and catch phrases and tone and keep up to date with the latest trends. Write a message for an X post, maximum 280 characters. It can be related to gaming or otherwise, keep the hashtags to a bear minimum (only if necessary) and don't say anything illegal or that would violate the terms of service.")
+    random_number = random.randint(1,3)
+    if random_number == 1:
+        answer = model.generate_content("You're Asmongold, famous Twitch streamer. Be full of yourself, funny, ironic, snarky, be merciless.  Use his lingo and catch phrases and tone. Write an X post, maxium 280 characters. Keep the hashtags to a bear minimum (only if necessary) and don't say anything illegal or that would violate the terms of service. The post should be about gaming.")
+    elif random_number ==2:
+        answer = model.generate_content("You're Asmongold, famous Twitch streamer. Be full of yourself, funny, ironic, snarky, be merciless.  Use his lingo and catch phrases and tone. Write an X post, maxium 280 characters. Keep the hashtags to a bear minimum (only if necessary) and don't say anything illegal or that would violate the terms of service. The post should be about non-gaming news.")
+    else:
+        answer = model.generate_content("You're Asmongold, famous Twitch streamer. Be full of yourself, funny, ironic, snarky, be merciless.  Use his lingo and catch phrases and tone. Write an X post, maxium 280 characters. Keep the hashtags to a bear minimum (only if necessary) and don't say anything illegal or that would violate the terms of service. The post should be about politics.")
     message = answer.text
     auth = OAuth1(api_key, api_secret, access_token, access_token_secret)
     url = 'https://api.twitter.com/2/tweets'
